cb510c233521efdcda151480947d2761e657e775
Maleine <maleinebabafaye@example.com>
2025-02-18 15:53:49 +0000

    verification des checkboes

--- main.js
+++ main.js
@@ -4,57 +4,57 @@ const form = document.getElementById("createForm");
 const nomElem = document.getElementById("nom");
 const prenomElem = document.getElementById("prenom");
 const gradeElem = document.getElementById("grade");
+//const moduleElem = document.getElementById("module");
+
 
 const checkboxMatieres = document.querySelectorAll("input[type=checkbox]");
-const checkboxError = document.getElementById("checkboxError");
+const checkboxError= document.getElementById("checkboxError");
 
-for (const checkbox of checkboxMatieres) {
-    checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
-            checkbox.classList.remove('is-invalid');
-            checkbox.classList.add('is-valid');
-        } else {
-            checkbox.classList.remove('is-valid');
-            checkbox.classList.add('is-invalid');
+
+//Array.form(checkboxMatieres).filter(checkbox => checkbox.checked);
+
+for(const checkbox of checkboxMatieres){
+    checkbox.addEventListener('change',() => {
+        if(checkbox.checked){
+            checkbox.classList.remove('is-invalid')
+            checkbox.classList.add('is-valid')
+        }else{
+            checkbox.classList.remove('is-valid')
+            checkbox.classList.add('is-invalid')
         }
-    });
+    })
 }
 
-const formFields = [nomElem, prenomElem, gradeElem];
+const formFields = [nomElem, prenomElem,gradeElem];
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    let formValid = true;
-
     for (const field of formFields) {
-        if (isEmpty(field)) {
+        if(isEmpty(field)){
             showErrorMessage(field);
-            formValid = false;
-        } else {
-            showSuccessMessage(field);
+            return; 
         }
+        showSuccessMessage(field);
     }
 
-    const elementsChoisis = document.querySelectorAll(".form-check-input:checked");
-    if (elementsChoisis.length == 0) {
-        checkboxError.textContent = "Veuillez cocher au moins un module";
-        checkboxError.classList.add('invalid-feedback');
-        formValid = false;
-    } else {
-        checkboxError.textContent = "";
-        checkboxError.classList.remove('invalid-feedback');
+    const elementsChoisis = document.querySelectorAll(".form-check-input:checked")
+    if (elementsChoisis.length == 0){
+        checkboxError.textContent = "veuillez cocher au moins un module";
+        return;
     }
 
-    if (formValid) {
-        const newProf = {
-            id: professeurs.length + 1,
-            nom: nomElem.value,
-            prenom: prenomElem.value,
-            grade: gradeElem.value,
-            module: Array.from(elementsChoisis).map(element => element.value)
-        };
-        saveDataProf(newProf);
-    }
+    checkboxError.textContent = "";
+
+    const newProf = { 
+        id: professeurs.length + 1, 
+        nom: nomElem.value, 
+        prenom: prenomElem.value,
+        grade: gradeElem.value,
+        module: Array.from(elementsChoisis).map(element => element.value)
+    };
+    saveDataProf(newProf);
+    
+
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -63,55 +63,63 @@ document.addEventListener("DOMContentLoaded", () => {
     genererDataProfesseur();
 });
 
+//const inputs = [nomElem, prenomElem,gradeElem,moduleElem];
+
 const inputs = document.getElementsByClassName('form-control');
 
-function activateFocus() {
+function activateFocus(){
     for (const input of inputs) {
         input.addEventListener('focus', () => {
             deleteClass(input, 'is-invalid', 'invalid-feedback');
             deleteClass(input, 'is-valid', 'valid-feedback');
-        });
+        })
     }
 }
 
+
 // Les fonctions de validation
 
-function isEmpty(champ) {
+function isEmpty(champ){
     return champ.value == '';
 }
 
-// Fonction d'affichage des messages de succès ou d'erreur sur les champs
+// Fonction d'affichage des messages de succès ou d"erreur sur les champs
 
-function showErrorMessage(champ) {
+function showErrorMessage(champ){
     const champError = document.getElementById(`${champ.id}Error`);
     champ.classList.add('is-invalid');
     champError.classList.add('invalid-feedback');
     champError.textContent = "Ce champ est obligatoire";
 }
 
-function showSuccessMessage(champ) {
+function showSuccessMessage(champ){
     const champError = document.getElementById(`${champ.id}Error`);
-    champ.classList.remove('is-invalid');
+    champ.classList.remove('is-invalid'); 
     champError.classList.remove('invalid-feedback');
 
     champ.classList.add('is-valid');
     champError.classList.add('valid-feedback');
+
     champError.textContent = '';
 }
 
-function deleteClass(champ, classInput, classError) {
+function deleteClass(champ, classInput, classError){
     const champError = document.getElementById(`${champ.id}Error`);
-    if (champ.classList.contains(classInput)) {
-        champ.classList.remove(classInput);
+    if(nomElem.classList.contains(classInput)){
+        champ.classList.remove(classInput); 
         champError.classList.remove(classError);
         champError.textContent = '';
     }
 }
 
+
+
+
 const tbody = document.getElementById("tbodyProfs");
 
 document.getElementById("btnOpenForm").addEventListener("click", openForm);
 document.getElementById("closeForm").addEventListener("click", closeForm);
+
 
 // Fonction d'accès aux données
 function genererDataProfesseur() {
@@ -133,14 +141,17 @@ function genererDataProfesseur() {
     });
 }
 
-function loadData() {
-    professeurs = localStorage.getItem("professeurs") != null ? JSON.parse(localStorage.getItem("professeurs")) : [];
+
+
+function loadData(){
+    professeurs = localStorage.key("professeurs") != null ? JSON.parse(localStorage.getItem("professeurs")) : [];
 }
+
 
 function saveDataProf(newProf) {
     professeurs.push(newProf);
     genererDataProfesseur();
-    closeForm();
+    closeForm('createForm');
 
     localStorage.setItem("professeurs", JSON.stringify(professeurs));
 }
@@ -160,12 +171,12 @@ function modifierProf(id) {
         prenomElem.value = prof.prenom;
         gradeElem.value = prof.grade;
 
-        // Ajouter la vérification pour les checkboxes
-        const moduleElems = document.querySelectorAll("input[type=checkbox]");
-        moduleElems.forEach((checkbox) => {
+        //ajouter la verification pour les checkboxes
+        const moduleElem = document.querySelectorAll("input[type=checkbox]")
+            moduleElem.forEach((checkbox) => {
             checkbox.checked = prof.module.includes(checkbox.value);
         });
-
+        
         form.style.display = "block";
 
         // Supprimer le professeur existant de la liste
